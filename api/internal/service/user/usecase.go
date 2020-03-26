bbeb20f8ec273bb69c5ded6995edf8af2fbe692c
Risky Feryansyah Pribadi <riskypribadi24@gmail.com>
2020-03-26 22:43:46 +0700

    add new test user usecase

--- api/internal/service/user/usecase.go
+++ api/internal/service/user/usecase.go
@@ -1,13 +1,12 @@
 package user
 
 import (
+	"context"
 	"github.com/confus1on/UKM/internal/model"
-
-	"github.com/valyala/fasthttp"
 )
 
 // UsecaseUser is inteface to implemented in usecase user
 type UsecaseUser interface {
-	CreateUser(ctx *fasthttp.RequestCtx, input model.InputCreateUser) (*model.ResponseRegister, error)
-	SigninUser(ctx *fasthttp.RequestCtx, input model.InputLoginUser) (*model.ResponseLogin, error)
+	CreateUser(ctx context.Context, input model.InputCreateUser) (*model.ResponseRegister, error)
+	SigninUser(ctx context.Context, input model.InputLoginUser) (*model.ResponseLogin, error)
 }
