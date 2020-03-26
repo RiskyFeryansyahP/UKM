b634004273a4766e27931f10741ddbdf11019efd
Risky Feryansyah Pribadi <riskypribadi24@gmail.com>
2020-03-26 11:40:19 +0700

    add new testing

---
+++ api/internal/service/user/repository/user_postgres_test.go
@@ -0,0 +1,137 @@
+package repository
+
+import (
+	"context"
+	"log"
+	"testing"
+	"time"
+
+	"github.com/confus1on/UKM/ent"
+	"github.com/confus1on/UKM/internal/model"
+
+	_ "github.com/mattn/go-sqlite3"
+	"github.com/stretchr/testify/require"
+)
+
+func TestUserRepository(t *testing.T) {
+	client, err := ent.Open("sqlite3", "file:ent?mode=memory&cache=shared&_fk=1")
+	if err != nil {
+		log.Fatalf("failed opening connection to sqlite: %v", err)
+	}
+	defer client.Close()
+
+	if err := client.Schema.Create(context.Background()); err != nil {
+		log.Fatalf("failed creating schema resources: %v", err)
+	}
+
+	t.Run("create user and profile", func(t *testing.T) {
+		now := time.Now()
+
+		profile := &ent.Profile{
+			ID:             1,
+			FirstName:      "Risky F",
+			LastName:       "Pribadi",
+			YearGeneration: "2017",
+			Phone:          "083834121715",
+			Status:         true,
+			ImgProfile:     "",
+			CreatedAt:      now,
+			UpdatedAt:      now,
+		}
+
+		user := model.InputCreateUser{
+			Email:       "171111040@mhs.stiki.ac.id",
+			Password:    "risky",
+			UserProfile: profile,
+		}
+
+		u := NewUserRepository(client)
+
+		_, err = u.Register(context.Background(), user)
+		require.NoError(t, err)
+	})
+
+	t.Run("fail create user if not enter email / password", func(t *testing.T) {
+		now := time.Now()
+
+		profile := &ent.Profile{
+			ID:             2,
+			FirstName:      "Teuku R",
+			LastName:       "Syahputra",
+			YearGeneration: "2017",
+			Phone:          "083834121713",
+			Status:         true,
+			ImgProfile:     "",
+			CreatedAt:      now,
+			UpdatedAt:      now,
+		}
+
+		input := model.InputCreateUser{
+			Email:       "",
+			Password:    "teuku",
+			UserProfile: profile,
+		}
+
+		user := NewUserRepository(client)
+
+		u, err := user.Register(context.Background(), input)
+		require.Error(t, err)
+		require.Nil(t, u)
+	})
+
+	t.Run("fail create profile if data not valid", func(t *testing.T) {
+		now := time.Now()
+
+		profile := &ent.Profile{
+			ID:             2,
+			FirstName:      "Bryan",
+			LastName:       "Pradana",
+			YearGeneration: "2016",
+			Phone:          "",
+			Status:         true,
+			ImgProfile:     "",
+			CreatedAt:      now,
+			UpdatedAt:      now,
+		}
+
+		input := model.InputCreateUser{
+			Email:       "171111041@mhs.stiki.ac.id",
+			Password:    "teuku",
+			UserProfile: profile,
+		}
+
+		user := NewUserRepository(client)
+
+		u, err := user.Register(context.Background(), input)
+		require.Error(t, err)
+		require.Nil(t, u)
+	})
+
+	t.Run("success login with valid email and password", func(t *testing.T) {
+		input := model.InputLoginUser{
+			Email:    "171111040@mhs.stiki.ac.id",
+			Password: "risky",
+		}
+
+		user := NewUserRepository(client)
+
+		profile, err := user.Login(context.Background(), input)
+		require.NoError(t, err)
+		require.Equal(t, "Risky F", profile.FirstName)
+		require.Equal(t, "Pribadi", profile.LastName)
+		require.Equal(t, true, profile.Status)
+	})
+
+	t.Run("failed login wrong email / password", func(t *testing.T) {
+		input := model.InputLoginUser{
+			Email:    "17111101@mhs.stiki.ac.id",
+			Password: "risky",
+		}
+
+		user := NewUserRepository(client)
+
+		profile, err := user.Login(context.Background(), input)
+		require.Error(t, err)
+		require.Nil(t, profile)
+	})
+}
