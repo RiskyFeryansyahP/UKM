c209128ed2b3ea40cf4dda90cb1010dafa2b0dd6
Risky Feryansyah Pribadi <riskypribadi24@gmail.com>
2020-04-02 23:28:53 +0700

    add get by email repository profile

--- api/internal/service/profile/repository.go
+++ api/internal/service/profile/repository.go
@@ -2,11 +2,13 @@ package profile
 
 import (
 	"context"
+
 	"github.com/confus1on/UKM/ent"
 	"github.com/confus1on/UKM/internal/model"
 )
 
 // RepositoryProfile is a interface to be implemented in profile repository
 type RepositoryProfile interface {
-	Update(ctx context.Context, id int, input model.InputUpdateProfile) (*ent.Profile, error)
+	GetByEmail(ctx context.Context, email string) (*ent.Profile, error)
+	Update(ctx context.Context, email string, input model.InputUpdateProfile) (*ent.Profile, error)
 }
