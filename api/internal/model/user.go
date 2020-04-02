9ae7a3faff71a91323424a048b34f1cee9f78719
Risky Feryansyah Pribadi <riskypribadi24@gmail.com>
2020-04-02 14:34:43 +0700

    refactor: moving input user to user model

--- api/internal/model/user.go
+++ api/internal/model/user.go
@@ -15,3 +15,23 @@ type InputLoginUser struct {
 	Email    string `json:"email"`
 	Password string `json:"password"`
 }
+
+// ResultRegister is model to show User after register
+type ResultRegister struct {
+	User *ent.User `json:"user"`
+}
+
+// ResponseRegister is model Response after successfull register
+type ResponseRegister struct {
+	StatusCode int       `json:"statuscode"`
+	Status     bool      `json:"status"`
+	Result     *ent.User `json:"result"`
+}
+
+// ResponseLogin model of response after login
+type ResponseLogin struct {
+	StatusCode int          `json:"statuscode"`
+	Status     bool         `json:""`
+	Profile    *ent.Profile `json:"result_profile"`
+	Role       *ent.Role    `json:"result_role"`
+}
