0d65596053e76c8fe28cf0f451c9876e0a53cfc6
Risky Feryansyah Pribadi <riskypribadi24@gmail.com>
2020-04-02 23:29:41 +0700

    add new get by email in repository profile

--- api/internal/service/profile/repository/profile_postgres.go
+++ api/internal/service/profile/repository/profile_postgres.go
@@ -5,10 +5,12 @@ import (
 	"time"
 
 	"github.com/confus1on/UKM/ent"
+	"github.com/confus1on/UKM/ent/user"
 	"github.com/confus1on/UKM/internal/model"
 	"github.com/confus1on/UKM/internal/service/profile"
 )
 
+// ProfileRepository repository of profile to connect database
 type ProfileRepository struct {
 	DB *ent.Client
 }
@@ -19,14 +21,23 @@ func NewProfileRepository(DB *ent.Client) profile.RepositoryProfile {
 }
 
 // Update is a update new profile
-func (p *ProfileRepository) Update(ctx context.Context, id int, input model.InputUpdateProfile) (*ent.Profile, error) {
+func (p *ProfileRepository) Update(ctx context.Context, email string, input model.InputUpdateProfile) (*ent.Profile, error) {
 	now := time.Now()
 
-	profile, err := p.DB.Profile.UpdateOneID(id).
+	id, err := p.DB.User.Query().
+		Where(user.Email(email)).
+		QueryProfile().
+		OnlyID(ctx)
+
+	if err != nil {
+		return nil, err
+	}
+
+	result, err := p.DB.Profile.UpdateOneID(id).
 		SetFirstName(input.FirstName).
 		SetLastName(input.LastName).
 		SetYearGeneration(input.YearGeneration).
-		SetPhone(input.YearGeneration).
+		SetPhone(input.Phone).
 		SetStatus(true).
 		SetUpdatedAt(now).
 		Save(ctx)
@@ -35,5 +46,18 @@ func (p *ProfileRepository) Update(ctx context.Context, id int, input model.Inpu
 		return nil, err
 	}
 
-	return profile, nil
+	return result, nil
+}
+
+// GetByEmail get detail profile by email
+func (p *ProfileRepository) GetByEmail(ctx context.Context, email string) (*ent.Profile, error) {
+	result, err := p.DB.User.Query().
+		Where(user.EmailEQ(email)).
+		QueryProfile().
+		Only(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	return result, nil
 }
