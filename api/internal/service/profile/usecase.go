5e0e82671829827ffef8f1df0ba0550e4598edd7
Risky Feryansyah Pribadi <riskypribadi24@gmail.com>
2020-04-02 23:29:05 +0700

    add new get profile in usecase

--- api/internal/service/profile/usecase.go
+++ api/internal/service/profile/usecase.go
@@ -2,10 +2,12 @@ package profile
 
 import (
 	"context"
+
 	"github.com/confus1on/UKM/internal/model"
 )
 
 // UsecaseProfile is a interface that will be implemented in profile usecase
 type UsecaseProfile interface {
-	UpdateProfile(ctx context.Context, id int, input model.InputUpdateProfile) (*model.ResponseUpdateProfile, error)
+	GetProfile(ctx context.Context, email string) (*model.ResponseGetProfile, error)
+	UpdateProfile(ctx context.Context, email string, input model.InputUpdateProfile) (*model.ResponseUpdateProfile, error)
 }
