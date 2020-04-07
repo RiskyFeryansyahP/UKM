116b93eaeea54de472d7474d42c0ebb3c9abbbc3
Risky Feryansyah Pribadi <riskypribadi24@gmail.com>
2020-04-07 11:52:26 +0700

    add server binding

---
+++ web/server.ts
@@ -0,0 +1,14 @@
+import express from 'express'
+import path from 'path'
+
+const app = express()
+const PORT = process.env.PORT || 5000
+
+app.use(express.static(path.join(__dirname, 'build')))
+
+// https://facebook.github.io/create-react-app/docs/deployment#serving-apps-with-client-side-routing
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'))
+})
+
+app.listen(PORT, () => console.log(`Listening on :${PORT}`))
